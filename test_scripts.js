c2d7b06db74f9fcf7e3090e5449d0bef9e44d099
Alana Harmon <alana.harmon@yahoo.com>
2022-08-19 13:10:25 -0500

    Update test_scripts.js

--- test_scripts.js
+++ test_scripts.js
@@ -1,6 +1,41 @@
 // CHECKBOX //
-import { CheckBox, Text, View } from "react-native";
+import { CheckBox, StyleSheet, Text, View } from "react-native";
 import { useState } from "react";
+
+export default function App() {
+  const [isSelected, setSelection] = useState(false);
+  return (
+    <View style={styles.container}>
+      <View style={styles.checkboxContainer}>
+      <CheckBox
+          value={isSelected}
+          onValueChange={setSelection}
+          style={styles.checkbox}
+        />
+        </View>
+        <Text>Is CheckBox selected: {isSelected ? "👍" : "👎"}</Text>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+  },
+  checkbox: {
+    alignSelf: "center",
+  },
+  label: {
+    margin: 8,
+  },
+});
+
 
 //SCREENS //
 
